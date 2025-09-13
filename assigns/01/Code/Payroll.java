c364f79b7282ddbaa0cef5164ef18dbeb2d3b298
Han Zheng <hanzheng@Hans-MacBook-Air-2.local>
2025-09-13 17:30:32 -0400

    Add TestPayroll and RunTests for assignment1

--- assigns/01/Code/Payroll.java
+++ assigns/01/Code/Payroll.java
@@ -4,30 +4,69 @@ public class Payroll {
 
     public Payroll() {
 	/* your code */
+        people = new Employee[INITIAL_MAXIMUM_SIZE];
+        maximum_size = INITIAL_MAXIMUM_SIZE;
+        current_size = 0;
     }
     
     public void add_employee(Employee newbie) {
 	/* your code */
+        if(current_size<maximum_size)
+        {
+            Employee[] temp = new Employee[maximum_size*2];
+            for(int i = 0;i<maximum_size;i++)
+            {
+                temp[i] = people[i];
+            }
+            people = temp;
+        }
+        current_size++;
+        people[current_size] = newbie;
     }
 
     public void remove_employee(int i) throws EmployeeIndexException {
 	/* your code */
+        if((i<0) || (i>=current_size))
+        {
+            throw new EmployeeIndexException();
+        }
+        for(int index=i;index<current_size-1;i++)
+        {
+            people[index] = people[index+1];
+        }
+        current_size--;
     }
     
     public int find_employee(String name) throws EmployeeNotFoundException {
 	/* your code */
+        for(int i = 0; i<current_size-1;i++)
+        {
+            if(people[i].name==name)
+            {
+                return i;
+            }
+        }
+        throw new EmployeeNotFoundException();
     }
 
     public void add_payroll(Payroll source) {
 	/* your code */
+        for(int i = 0;i<source.current_size;i++)
+        {
+            add_employee(source.people[i]);
+        }
     }
 
     public void copy_payroll(Payroll source) {
 	/* your code */
+        current_size = 0;
+        for(int i = 0;i<source.current_size;i++)
+        {
+            add_employee(source.people[i]);
+        }
     }
 
     private Employee people[];
     private int maximum_size;
     private int current_size;
-    ...
 }
--- assigns/01/Code/TestPayroll.java
+++ assigns/01/Code/TestPayroll.java
@@ -1,4 +1,28 @@
-
 public class TestPayroll {
     /* your code */
+    {
+        try {
+            Employee e1 = new Employee();
+            Employee e2 = new Employee();
+            Employee e3 = new Employee();
+
+            e1.name = "Adam";
+            e1.salary = 100;
+            e2.name = "Bob";
+            e2.salary = 100;
+            e3.name = "Charlie";
+            e3.salary = 100;
+
+            Payroll p1 = new Payroll();
+
+            p1.add_employee(e1);
+            p1.add_employee(e2);
+            p1.add_employee(e3);
+            
+            p1.remove_employee(1);  
+
+        } catch(EmployeeIndexException e) {
+            System.out.println("Error handling employees: " + e.getMessage());
+        }
+    }
 }
